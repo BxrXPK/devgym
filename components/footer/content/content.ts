a69ed0f281da61075d4114e8105275091bb8f32f
YasinUz <tyuzun@hotmail.de>
2023-07-05 11:03:56 +0200

    first commit, test

---
+++ components/footer/content/content.ts
@@ -0,0 +1,55 @@
+export const NavLinks = [
+    { href: '/', key: 'Home', text: 'Home' },
+    { href: '/courses', key: 'Courses', text: 'Courses' },
+    { href: '/about', key: 'About', text: 'About' },
+  ];
+  
+  export const categoryFilters = [
+    "Frontend",
+    "Backend",
+  ]
+  
+  export const footerLinks = [
+    {
+      title: 'Courses',
+      links: [
+        'HTML',
+        'CSS',
+        'Javascript',
+      ],
+    },
+    {
+      title: 'About Us',
+      links: [
+        'Our Team',
+        'More Projects',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'For Companies',
+      links: [
+        'Advertise with us',
+      ],
+    },
+    {
+      title: "For Parents",
+      links: [
+        'Safety for children',
+        'How to use this site as a parent',
+        'Disclaimer things'
+      ]
+    },
+
+    {
+      title: "Find us on:",
+      links: [
+        'Github',
+        'Facebook',
+        'Instagram',
+        'LinkedIn'
+      ]
+    }
+    
+  ];
+  
